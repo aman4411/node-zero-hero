f2498fb594723679aa62274bf331b016c4b275ff
Aman <amanmehta4411@gmail.com>
2021-08-20 10:11:58 +0530

    performed crud operations using oracle db

--- oracle/Employee.js
+++ oracle/Employee.js
@@ -11,6 +11,9 @@ const dbConfig = {
     connectString: ""
 }
 
+// ? Automatically commits db transactions...
+oracledb.autoCommit = true;
+
 app.use(cors())
 // ! To get the posted data add below two lines
 app.use(express.json())
@@ -45,6 +48,61 @@ router.get("/employees/:id",(req,res) => {
         
         const result = await connection.execute(`Select * from emp where ID = ${employeeId}`);
         res.json(result.rows);
+    })
+})
+
+// * CREATE Employee
+router.post("/employees", (req,res) => {
+    let{NAME,EMAIL,SALARY} = req.body;
+    oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+    oracledb.getConnection(dbConfig,async function(err,connection){
+        if(err){
+            console.log(`Oracle DB Connect Error : ` +err);
+        }
+        
+        const postQuery = `Insert into emp(name,email,salary) values(:1, :2, :3)`;
+        binds = [`${NAME}`, `${EMAIL}`, `${SALARY}` ];
+        const result = await connection.execute(postQuery, binds, {});
+        res.json(result.rows);
+        console.log("Number of Rows Inserted : " + result.rowsAffected);
+        await connection.close();
+    })
+})
+
+// * UPDATE existing employee
+router.put("/employees/:id", (req,res) => {
+    const employeeId = req.params.id;
+    let{NAME,EMAIL,SALARY} = req.body;
+    oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+    oracledb.getConnection(dbConfig,async function(err,connection){
+        if(err){
+            console.log(`Oracle DB Connect Error : ` +err);
+        }
+        
+        const updateQuery = `Update emp set name=:1,email=:2,salary=:3 where id=:4`;
+        binds = [`${NAME}`, `${EMAIL}`, `${SALARY}`, `${employeeId}` ];
+        const result = await connection.execute(updateQuery, binds, {});
+        res.json(result.rows);
+        console.log("Number of Rows Inserted : " + result.rowsAffected);
+        await connection.close();
+    })
+})
+
+// * DELETE existing employee
+router.delete("/employees/:id", (req,res) => {
+    const employeeId = req.params.id;
+    oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+    oracledb.getConnection(dbConfig,async function(err,connection){
+        if(err){
+            console.log(`Oracle DB Connect Error : ` +err);
+        }
+        
+        const updateQuery = `Delete from emp where id=:1`;
+        binds = [`${employeeId}` ];
+        const result = await connection.execute(updateQuery, binds, {});
+        res.json(result.rows);
+        console.log("Number of Rows Inserted : " + result.rowsAffected);
+        await connection.close();
     })
 })
 
